5ccbb62948bbf1c38d88f28c94500db7778aa82a
parthahluwalia <ahluwalia.p@husky.neu.edu>
2016-03-10 01:43:29 -0500

    Added packages - bodyparser, cookieparser

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.get('/', function (req, res) {
-    res.status(200).send('<h1>HELLO WORLD</h1>');
+    res.status(200).send('<h1>Adding some packages</h1>');
 });
 
 app.get('/foo', function(req, res){
