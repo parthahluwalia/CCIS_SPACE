61f27907b7f3a044d58a1023a4fdd47dfd6971d9
parthahluwalia <ahluwalia.p@husky.neu.edu>
2016-03-09 21:38:44 -0500

    / index route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
+app.get('/', function (req, res) {
+    res.status(200).send('<h1>HELLO WORLD!</h1>');
+});
+
 app.get('/foo', function(req, res){
     res.status(200).send('Foo Bar');
 });
