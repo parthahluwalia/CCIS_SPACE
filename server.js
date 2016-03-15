0e99eb1fa8ae8bdcf826ecb042b9bf43f64a7657
rishi11 <rishi11@ccs.neu.edu>
2016-03-15 17:25:41 -0400

    Test

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 6969;
 
 // Ummm, we need to get services based off of some config, how about creating a global service module like the one holding et.js, ses.js!
 var services = {};
