03e19d19c40d06215a502a5ff02d96d3aaed4ee0
rishi11 <rishi11@ccs.neu.edu>
2016-03-26 19:47:31 -0400

    Mongo DB Connection For Test Fail

--- test/booking.tests.js
+++ test/booking.tests.js
@@ -80,15 +80,15 @@ describe("Booking Tests", function(){
 
 
     it("A Booking should be made on api/booking/ POST", function(done){
-            chai.request(server)
+            return chai.request(server)
                 .post('/api/booking')
                 .send({"purpose": "Purpose 4", "startDate": "2016-03-20", "endDate": "2016-03-22", "startTime": "01:30", "endTime": "03:30", "requestor": { "email": "jannunzi@gmail.com" }, "roomNumber": "111"})
                 .end(function(err, res){
                     //res.should.have.status(200);
                     res.should.be.json;
                     //console.log(res.body);
-                    done();
-                })
+                    return done();
+                });
         });
 
 });
