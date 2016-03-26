a27fc9156659c0048f6f52a026fb2d410337bdf5
rishi11 <rishi11@ccs.neu.edu>
2016-03-26 18:25:13 -0400

    Mongo DB Connection For Test Fail

--- test/booking.tests.js
+++ test/booking.tests.js
@@ -22,11 +22,11 @@ describe("Booking Tests", function(){
     //
     //});
     //
-    //it("A server should create booking and return with newly added booking on /api/booking POST", function(){
-    //    //TO-DO
-    //    expect(true).to.be.true;
-    //
-    //})
+    it("A server should create booking and return with newly added booking on /api/booking POST", function(){
+        //TO-DO
+        expect(true).to.be.true;
+
+    });
 
     //it("A server should respond with status 200 on /api/booking GET", function(){
     //    chai.request(server)
@@ -72,10 +72,10 @@ describe("Booking Tests", function(){
                 .post('/api/booking')
                 .send({"purpose": "Purpose 4", "startDate": "2016-03-20", "endDate": "2016-03-22", "startTime": "01:30", "endTime": "03:30", "requestor": { "email": "jannunzi@gmail.com" }, "roomNumber": "111"})
                 .end(function(err, res){
-                    console.log("Error : ");
-                    console.log(err);
-                    console.log("Response : ");
-                    console.log(res);
+                    //console.log("Error : ");
+                    //console.log(err);
+                    //console.log("Response : ");
+                    //console.log(res);
                     //res.should.have.status(200);
                     //res.should.be.json;
                     //console.log(res.body);
