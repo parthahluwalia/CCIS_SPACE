c4aa123b6a1219bf22e17177fcb2118f849411f2
rishi11 <rishi11@ccs.neu.edu>
2016-03-27 00:59:50 -0400

    no test

--- test/booking.tests.js
+++ test/booking.tests.js
@@ -87,7 +87,7 @@ describe("Booking Tests", function(){
                 .post('/api/booking')
                 .send({"purpose": "Purpose 4", "startDate": "2016-03-20", "endDate": "2016-03-22", "startTime": "01:30", "endTime": "03:30", "requestor": { "email": "jannunzi@gmail.com" }, "roomNumber": "111"})
                 .end(function(err, res){
-                    //res.should.be.json;
+                    // res.should.be.json;
                  done();
                 })
         });
