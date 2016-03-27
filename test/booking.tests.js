bf7f95fd9c956ed0b6833deba33bcde95f75ac8a
rishi11 <rishi11@ccs.neu.edu>
2016-03-27 01:14:30 -0400

    Opening Booking Module

--- test/booking.tests.js
+++ test/booking.tests.js
@@ -82,15 +82,16 @@ describe("Booking Tests", function(){
 
 
     it("A Booking should be made on api/booking/ POST", function(done){
-        console.log("Called ");
+        //console.log("Called ");
              chai.request(server)
                 .post('/api/booking')
                 .send({"purpose": "Purpose 4", "startDate": "2016-03-20", "endDate": "2016-03-22", "startTime": "01:30", "endTime": "03:30", "requestor": { "email": "jannunzi@gmail.com" }, "roomNumber": "111"})
                 .end(function(err, res){
                     // res.should.be.json;
+                    console.log(res.error);
                  done();
                 })
         });
 
 
-});
+})
