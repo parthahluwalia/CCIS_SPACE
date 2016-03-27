83aba87d918f870732fbe039d6a73e7cdb9cab20
Neha Singh <nehasingh189@ccs.neu.edu>
2016-03-26 22:13:15 -0400

    ...
    ...

--- xml_parse.sh
+++ xml_parse.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-INPUT_FILE = "test-reports.xml"
+INPUT_FILE="test-reports.xml"
 DUP_FILE="dup-file.xml"
 OUTPUT_FILE="test-reports.xml"
 IFS=$'\n'
 
-	for i in $INPUT_FILE
-	do
-	cat test-report.xml | grep -e "<*>" >> $DUP_FILE
-	done
-	cat $DUP_FILE >> $OUTPUT_FILE
-	
-
+ for i in $INPUT_FILE
+ do
+ cat $INPUT_FILE | grep -e "<*>" >> $DUP_FILE
+ done
+ cat $DUP_FILE >> $OUTPUT_FILE
